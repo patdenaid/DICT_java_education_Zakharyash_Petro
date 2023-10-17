45e955c157fff2899286bc8a13b33ffa084c5ad3
patdenaid <zakharyshpetya@gmail.com>
2023-10-17 13:09:20 +0300

    ChatBot 7-th stage

--- src/Hangman/Hangman.java
+++ src/Hangman/Hangman.java
@@ -8,6 +8,23 @@ public class Hangman {
 
         System.out.println("HANGMAN");
 
+        while (true) {
+            System.out.println("Type \"play\" to play the game, \"exit\" to quit:");
+            String command = scanner.nextLine();
+
+            if (command.equals("exit")) {
+                break;
+            } else if (command.equals("play")) {
+                playHangman(scanner, random);
+            } else {
+                System.out.println("Unknown command. Type \"play\" to play the game, \"exit\" to quit.");
+            }
+        }
+
+        System.out.println("Goodbye!");
+    }
+
+    private static void playHangman(Scanner scanner, Random random) {
         String[] words = {"python", "java", "javascript", "kotlin"};
         int randomIndex = random.nextInt(words.length);
         String wordToGuess = words[randomIndex];
@@ -23,14 +40,12 @@ public class Hangman {
             System.out.println("Input a letter: >");
             String inputLetter = scanner.nextLine();
 
-            if (wordToGuess.contains(inputLetter)) {
-                // Буква зустрічається в слові
+            if (inputLetter.length() != 1 || !Character.isLowerCase(inputLetter.charAt(0))) {
+                System.out.println("Please enter a lowercase English letter");
+            } else if (wordToGuess.contains(inputLetter)) {
                 if (wordInProgress.toString().contains(inputLetter)) {
-                    // Гравець вже намагався вгадати цю букву
-                    System.out.println("No improvements");
-                    lives--;
+                    System.out.println("You've already guessed this letter");
                 } else {
-                    // Відкрити букву у слові
                     for (int i = 0; i < wordToGuess.length(); i++) {
                         if (wordToGuess.charAt(i) == inputLetter.charAt(0)) {
                             wordInProgress.setCharAt(i, inputLetter.charAt(0));
@@ -38,25 +53,20 @@ public class Hangman {
                     }
                 }
             } else {
-                // Буква не зустрічається в слові
                 System.out.println("That letter doesn't appear in the word");
                 lives--;
             }
 
             if (!wordInProgress.toString().contains("-")) {
-                // Гравець вгадав всі літери
                 System.out.println(wordInProgress);
-                System.out.println("You guessed the word!");
+                System.out.println("You guessed the word " + wordToGuess + "!");
                 System.out.println("You survived!");
                 break;
             }
         }
 
         if (lives == 0) {
-            // Гравець витратив всі життя
             System.out.println("You lost!");
         }
-
-        scanner.close();
     }
 }
