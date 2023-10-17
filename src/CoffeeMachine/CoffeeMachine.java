842fd9f9360f931d8b60e9689378e00e3fc138cd
patdenaid <zakharyshpetya@gmail.com>
2023-10-17 13:24:33 +0300

    CoffeeMachine 3-rd stage

--- src/CoffeeMachine/CoffeeMachine.java
+++ src/CoffeeMachine/CoffeeMachine.java
@@ -4,17 +4,32 @@ public class CoffeeMachine {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
+        System.out.println("Write how many ml of water the coffee machine has:");
+        int waterAvailable = scanner.nextInt();
+
+        System.out.println("Write how many ml of milk the coffee machine has:");
+        int milkAvailable = scanner.nextInt();
+
+        System.out.println("Write how many grams of coffee beans the coffee machine has:");
+        int coffeeBeansAvailable = scanner.nextInt();
+
         System.out.println("Write how many cups of coffee you will need:");
-        int cups = scanner.nextInt();
+        int cupsNeeded = scanner.nextInt();
 
-        int water = cups * 200;
-        int milk = cups * 50;
-        int coffeeBeans = cups * 15;
+        int waterRequired = cupsNeeded * 200;
+        int milkRequired = cupsNeeded * 50;
+        int coffeeBeansRequired = cupsNeeded * 15;
 
-        System.out.println("For " + cups + " cups of coffee you will need:");
-        System.out.println(water + " ml of water");
-        System.out.println(milk + " ml of milk");
-        System.out.println(coffeeBeans + " g of coffee beans");
+        int cupsAvailable = Math.min(waterAvailable / 200, Math.min(milkAvailable / 50, coffeeBeansAvailable / 15));
+
+        if (cupsAvailable == cupsNeeded) {
+            System.out.println("Yes, I can make that amount of coffee");
+        } else if (cupsAvailable > cupsNeeded) {
+            int extraCups = cupsAvailable - cupsNeeded;
+            System.out.println("Yes, I can make that amount of coffee (and even " + extraCups + " more than that)");
+        } else {
+            System.out.println("No, I can make only " + cupsAvailable + " cups of coffee");
+        }
 
         scanner.close();
     }
