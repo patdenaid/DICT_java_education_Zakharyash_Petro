974c28d62ff361debec913cbcfd1655c4b22dcab
patdenaid <zakharyshpetya@gmail.com>
2023-10-17 12:59:57 +0300

    Hangman 1-st stage

---
+++ src/Hangman/Hangman.java
@@ -0,0 +1,6 @@
+public class Hangman {
+    public static void main(String[] args) {
+        System.out.println("HANGMAN");
+        System.out.println("The game will be available soon.");
+    }
+}
