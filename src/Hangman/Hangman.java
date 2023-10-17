2a0a70407e647a5fe22a0ecc5c53f086293c6729
patdenaid <zakharyshpetya@gmail.com>
2023-10-17 13:02:12 +0300

    Hangman 3-rd stage

--- src/Hangman/Hangman.java
+++ src/Hangman/Hangman.java
@@ -1,13 +1,22 @@
 import java.util.Scanner;
+import java.util.Random;
 
 public class Hangman {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        Random random = new Random();
 
         System.out.println("HANGMAN");
+
+        // Список слів для вибору
+        String[] words = {"python", "java", "javascript", "kotlin"};
+
+        // Випадковим чином вибираємо слово зі списку
+        int randomIndex = random.nextInt(words.length);
+        String wordToGuess = words[randomIndex];
+
         System.out.println("Guess the word: >");
 
-        String wordToGuess = "java"; // Слово, яке гравцеві потрібно вгадати
         String playerGuess = scanner.nextLine();
 
         if (playerGuess.equals(wordToGuess)) {
