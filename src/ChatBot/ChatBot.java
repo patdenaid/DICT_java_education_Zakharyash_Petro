1eb6b4fda500cd8be815390176152007bd92e524
patdenaid <zakharyshpetya@gmail.com>
2023-10-17 12:42:16 +0300

    ChatBot 2-nd stage

--- src/ChatBot/ChatBot.java
+++ src/ChatBot/ChatBot.java
@@ -1,9 +1,17 @@
+import java.util.Scanner;
+
 public class ChatBot {
     public static void main(String[] args) {
         String botName = "ChatBot"; // Встановіть ім'я бота за вашим вибором
-        int currentYear = java.time.Year.now().getValue(); // Отримуємо поточний рік
+        int birthYear = java.time.Year.now().getValue(); // Отримуємо поточний рік
 
         System.out.println("Hello! My name is " + botName + ".");
-        System.out.println("I was created in " + currentYear + ".");
+        System.out.println("I was created in " + birthYear + ".");
+        System.out.println("Please, remind me your name.");
+
+        Scanner scanner = new Scanner(System.in);
+        String userName = scanner.nextLine();
+
+        System.out.println("What a great name you have, " + userName + "!");
     }
 }
