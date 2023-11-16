2f226b80ba072e8e4bbd2b68214a9ef64c3311f5
patdenaid <zakharyashpetya@gmail.com>
2023-11-16 20:23:45 +0200

    TicTacToe second stage

--- src/TicTacToe/TicTacToe.java
+++ src/TicTacToe/TicTacToe.java
@@ -1,23 +1,41 @@
+import java.util.Scanner;
+
 public class TicTacToe {
 
     // Функція для друку ігрового поля
     private static void printBoard(char[][] board) {
+        System.out.println("---------");
         for (char[] row : board) {
+            System.out.print("| ");
             for (char cell : row) {
                 System.out.print(cell + " ");
             }
-            System.out.println();
+            System.out.println("|");
         }
+        System.out.println("---------");
     }
 
     public static void main(String[] args) {
-        // Приклад використання
-        char[][] exampleBoard = {
-                {'X', 'O', 'X'},
-                {'O', 'X', 'O'},
-                {'X', 'X', 'O'}
-        };
+        Scanner scanner = new Scanner(System.in);
 
-        printBoard(exampleBoard);
+        System.out.print("Enter cells: ");
+        String input = scanner.nextLine();
+
+        char[][] gameBoard = new char[3][3];
+        int index = 0;
+
+        // Заповнення ігрового поля з введеного рядка
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                gameBoard[i][j] = input.charAt(index);
+                index++;
+            }
+        }
+
+        // Друк ігрового поля
+        printBoard(gameBoard);
+
+        // Закриття сканера
+        scanner.close();
     }
 }
