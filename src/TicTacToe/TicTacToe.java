e3e3dca8bae845a124d176bd1e0fb8284be370f3
patdenaid <zakharyashpetya@gmail.com>
2023-11-16 20:36:06 +0200

    TicTacToe fourth stage

--- src/TicTacToe/TicTacToe.java
+++ src/TicTacToe/TicTacToe.java
@@ -2,7 +2,6 @@ import java.util.Scanner;
 
 public class TicTacToe {
 
-    // Функція для друку ігрового поля
     private static void printBoard(char[][] board) {
         System.out.println("---------");
         for (char[] row : board) {
@@ -15,9 +14,7 @@ public class TicTacToe {
         System.out.println("---------");
     }
 
-    // Функція для перевірки можливості перемоги для символу на ігровому полі
     private static boolean checkWin(char[][] board, char symbol) {
-        // Перевірка рядків і стовпців
         for (int i = 0; i < 3; i++) {
             if ((board[i][0] == symbol) && (board[i][1] == symbol) && (board[i][2] == symbol)) {
                 return true; // Перемога у рядку
@@ -27,7 +24,6 @@ public class TicTacToe {
             }
         }
 
-        // Перевірка діагоналей
         if ((board[0][0] == symbol) && (board[1][1] == symbol) && (board[2][2] == symbol)) {
             return true; // Перемога на головній діагоналі
         }
@@ -38,13 +34,25 @@ public class TicTacToe {
         return false;
     }
 
-    // Функція для аналізу стану гри
+    private static boolean isValidMove(char[][] board, int row, int col) {
+        if (row < 0 || row >= 3 || col < 0 || col >= 3) {
+            System.out.println("Coordinates should be from 1 to 3!");
+            return false;
+        }
+
+        if (board[row][col] != ' ') {
+            System.out.println("This cell is occupied! Choose another one!");
+            return false;
+        }
+
+        return true;
+    }
+
     private static void analyzeGameState(char[][] board) {
         int countX = 0;
         int countO = 0;
         int countEmpty = 0;
 
-        // Підрахунок кількості "X", "O" та порожніх клітинок
         for (int i = 0; i < 3; i++) {
             for (int j = 0; j < 3; j++) {
                 if (board[i][j] == 'X') {
@@ -57,11 +65,9 @@ public class TicTacToe {
             }
         }
 
-        // Перевірка можливості перемоги
         boolean xWins = checkWin(board, 'X');
         boolean oWins = checkWin(board, 'O');
 
-        // Аналіз стану гри та виведення результату
         if (Math.abs(countX - countO) >= 2 || (xWins && oWins)) {
             System.out.println("Impossible");
         } else if (xWins) {
@@ -84,7 +90,6 @@ public class TicTacToe {
         char[][] gameBoard = new char[3][3];
         int index = 0;
 
-        // Заповнення ігрового поля з введеного рядка
         for (int i = 0; i < 3; i++) {
             for (int j = 0; j < 3; j++) {
                 gameBoard[i][j] = input.charAt(index);
@@ -92,13 +97,33 @@ public class TicTacToe {
             }
         }
 
-        // Друк ігрового поля
         printBoard(gameBoard);
 
-        // Аналіз стану гри
-        analyzeGameState(gameBoard);
+        boolean validMove = false;
+        do {
+            System.out.print("Enter the coordinates: ");
+            try {
+                int row = scanner.nextInt() - 1;
+                int col = scanner.nextInt() - 1;
 
-        // Закриття сканера
+                if (isValidMove(gameBoard, row, col)) {
+                    gameBoard[row][col] = 'X';
+                    validMove = true;
+                }
+            } catch (Exception e) {
+                System.out.println("You should enter numbers!");
+                scanner.nextLine();
+            }
+        } while (!validMove);
+
+        printBoard(gameBoard);
+
+        if (checkWin(gameBoard, 'X')) {
+            System.out.println("X wins");
+        } else {
+            analyzeGameState(gameBoard);
+        }
+
         scanner.close();
     }
 }
